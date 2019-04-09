4c3c0f6856b0f724f15b6b1059be15ae197c969e
decantr <theeasterlywood@gmail.com>
2019-04-09 18:12:17 +0100

    change database name

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_NAME', 'testing');
 
 /* attempt connection */
 try{
     $db = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
 } catch(PDOException $e){
-    die(":: ERROR: Could not connect. " . $e->getMessage());
+    die(":: ERROR : Could not connect. " . $e->getMessage());
 }
 ?>
