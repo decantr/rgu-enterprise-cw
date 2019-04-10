020f3fc8816de363733ca9c3f7255ba7d67cd6ad
decantr <theeasterlywood@gmail.com>
2019-04-10 19:20:48 +0100

    add table for that will hold results

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <?
 
-if (!isset($_COOKIE["session"])) {
-	header("Location: login.php");
-	exit;
+$loggedin = false;
+if (isset($_COOKIE["session"])) {
+	$loggedin = true;
 }
 
 ?>
 
+<!-- js     -->
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script>
+feedreq=new XMLHttpRequest();
+feedreq.onreadystatechange=() => {
+	if (this.status==200)
+		document.getElementById("placeholder").innerHTML=this.responseText;
+}
+feedreq.open("GET","feedreq.php",true);
+feedreq.send();
+</script>
+
+<!-- header -->
+
 <h5>under const</h5>
-<div><p>feed 1</p><div>
+
+<!-- content -->
+
+<? if (! $loggedin) { ?>
+	please log in <a href="login.php">here</a>
+<? } ?>
+<table><thead>
+<tr>
+	<td><b>Date</b></td>
+	<td><b>Description</b></td>
+	<td><b>Channel</b></td>
+	<td align="right"><b>Star</b></td>
+	<td align="right"><b>Hide</b></td>
+</tr>
+</thead><tbody>
+<div id="placeholder"></div>
+<tr>
+	<td></td>
+	<td id="bottomText" colspan="4">RSS-feed will be displayed here [...]</td>
+</tr>
+</tbody></table>
+
