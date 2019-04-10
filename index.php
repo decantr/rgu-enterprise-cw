f9c84db9f0295c44e0f6fea163f7a5313d8ad999
decantr <theeasterlywood@gmail.com>
2019-04-10 23:43:14 +0100

    parse json and create table entries

--- index.php
+++ index.php
@@ -22,6 +22,27 @@ fetch( "feedreq.php" )
 
 }
 
+function display(content) {
+
+	let str = "";
+	let options = {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'};
+
+	for ( let i of content ) {
+		let time = new Date(Date.parse(i.date))
+		time = time.toLocaleString('en-GB', options)
+		let summary = i.desc.replace(/<(?:.|\n)*?>/gm, '').slice(0, 20) + "..."
+		let title = i.title.replace(/<(?:.|\n)*?>/gm, '').slice(0, 64)
+
+		str += "<tr><td>" + time +
+			"</td><td><a href=" +i.link +
+			">" + title + "</a></td><td>" +
+			summary + "</td><td align=\"right\">" +
+			"Star</td><td align=\"right\">Hide</td></tr>"
+	}
+
+	document.getElementById("posttable").innerHTML=str;
+
+}
 </script>
 
 <!-- header -->
