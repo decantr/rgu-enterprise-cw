e564ab515e465e1b1919aa971ac33c56f5afad39
decantr <theeasterlywood@gmail.com>
2019-04-10 09:23:28 +0100

    add barebones login

--- login.php
+++ login.php
@@ -1,8 +1,35 @@
 <?
-
 if ( isset( $_COOKIE["session"] ) ) {
 	header("Location: /index.php");
 	exit;
+}
+
+// if this is a login request
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+require_once "config.php";
+
+if ( empty( trim( $_POST["username"] ) ) || empty( trim( $_POST["password"] ) ) ) {
+	exit;
+}
+
+$request = "select `username`, `password` from `users` where `username` = :username";
+$statement = $db->prepare($request);
+$statement->execute([":username" => $_POST["username"]]);
+
+if ( $statement->rowCount() == 0 ){
+	exit;
+}
+
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+
+if ( $_POST["password"] == $result["password"] ) {
+	$token = bin2hex( random_bytes(32) );
+
+	setcookie("session", $token);
+	header("Location: /index.php");
+	exit();
+}
 }
 
 ?>
