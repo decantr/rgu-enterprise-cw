d142d98f31b4b0294fe3ba15f9c2287bbb9d88a4
decantr <theeasterlywood@gmail.com>
2019-04-11 16:28:44 +0100

    start working on multiple feeds

--- feedreq.php
+++ feedreq.php
@@ -9,15 +9,15 @@ class Post {
 
 	public function __construct( $t, $d, $l, $c ) {
 		$this->title = (string) $t;
-		$this->date = $d;
+		$this->date = (string) $d;
 		$this->link = (string) $l;
 		$this->desc = (string) $c;
 	}
 }
 
-//$url = 'https://lukesmith.xyz/rss.xml';
-$url = 'lukesmith.xyz/rss.xml';
-$feed = simplexml_load_file($url);
+// function to return the json of the feed
+function getFeed( $feedurl ) {
+$feed = simplexml_load_file($feedurl);
 $posts = array();
 
 // iterate through all of the items
@@ -25,7 +25,7 @@ foreach ($feed->channel->item as $item) {
 
 	$post = new Post(
 		$item->title,
-		(strtotime( $item->pubDate) ),
+		$item->pubDate,
 		$item->link != "" ? $item->link : $item->guid,
 		$item->description
 	);
@@ -33,6 +33,33 @@ foreach ($feed->channel->item as $item) {
 
 }
 
-// return the array of posts as a json object
-echo json_encode($posts);
+// return the array of posts
+return $posts;
+}
+
+function buildFeed( $quantity ) {
+	// FOR TESTING
+	//$url = 'https://lukesmith.xyz/rss.xml';
+	$testUrl = 'lukesmith.xyz/rss.xml';
+	// TODO : get the urls from the users DB
+	// TODO : select handle the amount from the user
+	// TODO : select the time ordered top # of feeds
+
+	$feeds = array();
+	// get the urls for the user
+	$user_subscriptions = array();
+	$user_subscriptions[0] = $testUrl;
+	$user_subscriptions[1] = $testUrl;
+
+	foreach ( $user_subscriptions as $url ) {
+		$feeds = getFeed( $url );
+	}
+
+	// TODO : sort the arrays by chronology
+	$topFeeds = array_slice($feeds, 0, $quantity);
+	return $topFeeds;
+}
+
+echo json_encode(buildFeed( 10 ));
+
 ?>
