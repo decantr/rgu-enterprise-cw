ef60e22ae0c6067788bb8c27cbc86960e0928f46
decantr <theeasterlywood@gmail.com>
2019-04-12 20:59:44 +0100

    check if already subscribed and readd error

--- manage.php
+++ manage.php
@@ -2,6 +2,7 @@
 
 require_once "lib/config.php";
 require_once "lib/Feed.php";
+$error = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -9,6 +10,8 @@ $feedurl = trim ( $_POST["feedurl"] );
 
 if ( empty ( $feedurl ) ) {
 	$error = "No feed given";
+} else if (filter_var($feedurl, FILTER_VALIDATE_URL) === false) {
+	$error = "Not a valid url";
 } else {
 	$exists = $db->prepare("SELECT * FROM feeds WHERE `link` = :link LIMIT 0, 1");
 	$exists->execute( [":link" => $feedurl ] );
@@ -25,13 +28,25 @@ if ( $exists->rowCount() != 0 ) {
 	$statement->execute( [":title" => $feed->title, ":summary" => $feed->summary, ":link" => $feed->link] );
 	$feed->id = $db->lastInsertId();
 }
-$request = "INSERT INTO `subscriptions` (`user_id`, `feed_id`) VALUES (:user_id, :feed_id)";
-$statement = $db->prepare( $request );
-$statement->execute([":user_id" => $_SESSION["user_id"], ":feed_id" => $feed->id]);
-$error = "Feed successfully created";
+
+// test if the feed is already subscribed
+$exists = $db->prepare( "SELECT * FROM subscriptions WHERE user_id = :user_id AND feed_id = :feed_id" );
+$exists->execute([":user_id" => $_SESSION["user_id"], ":feed_id" => $feed->id]);
+echo "hello " . $exists->rowCount();
+
+if ( $exists->rowCount() != 0 ) {
+	$error = "already subscribed";
+} else {
+	// insert the subscription into the db
+	$request = "INSERT INTO `subscriptions` (`user_id`, `feed_id`) VALUES (:user_id, :feed_id)";
+	$statement = $db->prepare( $request );
+	$statement->execute([":user_id" => $_SESSION["user_id"], ":feed_id" => $feed->id]);
+	$error = "Feed successfully created";
 }
 }
-	//$error = "Feed already exists at " . (string) $exists["id"];
+}
+//$error = "Feed already exists at " . (string) $exists["id"];
+$error = $error == "" ? $error : $error . "<br />";
 ?>
 
 <!--	imports	-->
@@ -65,7 +80,9 @@ $error = "Feed successfully created";
 <h1>Add New Feed</h1>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
 	<label for="feedurl">Feed Url</label>
-	<input type="text" name="feedurl" minlength="8" required>
+	<input type="text" name="feedurl" required>
 	<input type="submit" value="Add" name="add">
 </form>
+<br />
+<span class="error"><?php echo $error ?></span>
 </body>
