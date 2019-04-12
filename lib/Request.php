6aa62dda35564b05499d27e374f2fbb9ee9aecc9
decantr <theeasterlywood@gmail.com>
2019-04-12 23:50:21 +0100

    working towards caching

--- lib/Request.php
+++ lib/Request.php
@@ -1,5 +1,6 @@
 <? session_start();
 
+
 switch ($_SERVER['QUERY_STRING']) {
 case "q=getSubscribedFeeds":
 	echo json_encode(getSubscribedFeeds());
@@ -12,8 +13,6 @@ default;
 }
 
 function getSubscribedFeeds() {
-require_once "config.php";
-require_once "Feed.php";
 
 $sql = "SELECT `feeds`.`id`, `feeds`.`title`, `feeds`.`summary`, `feeds`.`link`, `feeds`.`updated` FROM `feeds` INNER JOIN `subscriptions` ON `subscriptions`.`feed_id` = `feeds`.`id` WHERE `subscriptions`.`user_id` = :user_id";
 
@@ -28,44 +27,67 @@ while ( $row = $statement->fetch(PDO::FETCH_ASSOC) )
 return $feeds;
 }
 
-function getArticles() {
+function getTopArticles() {
+	require_once "config.php";
+	require_once "Feed.php";
 	require_once "Article.php";
 
 	$subscriptions = getSubscribedFeeds();
 	$topFeeds = array();
 
-	foreach ( $subscriptions as $feed) {
-		array_push( $topFeeds, getFeed( $feed->link ) );
+	$request = "SELECT `articles`.`id`, `articles`.`feed_id`, `articles`.`title`, `articles`.`description`, `articles`.`link`, `articles`.`date`
+FROM `articles`
+INNER JOIN `subscriptions` ON `subscriptions`.`feed_id` = `articles`.`feed_id`
+WHERE `subscriptions`.`user_id` = :user_id
+ORDER BY `articles`.`date` DESC";
+
+	$statement = $db->prepare( $request );
+	$statement->execute([":user_id" => $session["user_id"]]);
+
+	while ($row = $statement->fetch( PDO::FETCH_ASSOC )) {
+		array_push( $topFeeds, getFeed( $feed ) );
 	}
 
 	return $topFeeds;
 }
 
-function getFeed( $feedLink ) {
-$feed = simplexml_load_file( $feedLink );
+function getFeed( $f ) {
+$feed = simplexml_load_file( $f->link );
 $articles = array();
-$source = (string) $feed->channel->title;
 
 // iterate through all of the items
-$count = 0;
 foreach ( $feed->channel->item as $item ) {
-	if ( $count > 10 ) break;
-	$article = Article::articleFromItem( $item , $source );
+	$article = Article::articleFromItem( $item , $f->id );
 
 	try {
 		json_encode($article);
 	} catch ( Exception $e ) {
 		$article = null;
+		break;
 	}
+
+	updateArticle( $article );
 
 	if ( $article != null )
 		array_push( $articles, $article );
 
-	$count++;
 }
 
 // return the array of posts
 return $articles;
 }
+
+function updateArticle( $article ) {
+
+	$insert = $db->prepare("INSERT INTO `articles` (`feed_id`, `title`, `summary`, `link`, `published`) VALUES (:feed_id, :title, :summary, :link, :published)");
+
+	$insert->execute([
+		":feed_id" => $row["id"],
+		":title" => $article["title"],
+		":summary" => $article["description"],
+		":link" => $article["link"],
+		":published" => $article["date"]->format("Y-m-d H:i:s"),
+	]);
+	}
 
 ?>
